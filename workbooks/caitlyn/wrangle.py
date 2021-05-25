9ec01cbb85e3142748dbf32eb4be156cba2000eb
Caitlyn Carney <caitlyn.r.carney@gmail.com>
2021-05-25 08:37:33 -0500

    Acquire and Prepare Functions

--- workbooks/caitlyn/wrangle.py
+++ workbooks/caitlyn/wrangle.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.model_selection import train_test_split
+import sklearn.preprocessing
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+#-----------------------------------------------------------------------------
+
+def get_311_data():
+    '''
+    This function uses pandas read .csv to read in the downloaded .csv 
+    from: https://data.sanantonio.gov/dataset/service-calls/resource/20eb6d22-7eac-425a-85c1-fdb365fd3cd7
+    after the .csv is read in, it returns it as a data frame.
+    '''
+    df= pd.read_csv('service_calls.csv')
+    return df
 #-----------------------------------------------------------------------------
 
 # Set the index
@@ -21,7 +33,7 @@ def drop_and_index(df):
                  'Report Starting Date', 
                  'Report Ending Date' ], inplace=True)
     # Set index to case id
-    df.set_index('CASEID')
+    df.set_index('CASEID', inplace=True)
     return df
 
 #-----------------------------------------------------------------------------
@@ -76,11 +88,35 @@ def create_delay_columns(df):
                                 labels = ['Extremely Late Response', 'Very Late Response', 
                                           'Late Response', "On Time Response", "Early Response", 
                                           'Very Early Response', 'Extremely Early Response'])
-    # add a new category for level of delay
-    df["level_of_delay"] = df['level_of_delay'].cat.add_categories('Still Open')
-    # replace nulls in level of delay with the new "Still Open" category
-    df["level_of_delay"].fillna("Still Open", inplace=True)
+    # drop nulls in these columns
+    df.dropna(subset=['days_open'], how='all', inplace=True)
+    df.dropna(subset=['level_of_delay'], how='all', inplace=True)
+    
     # return new df
+    return df
+
+#-----------------------------------------------------------------------------
+
+def handle_outliers(df):
+    '''removes outiers from df'''
+    # remove outliers from days_open
+    df = df[df.days_open < 1400]
+    # return df
+    return df
+
+#-----------------------------------------------------------------------------
+
+def create_dummies(df):
+    '''This function creates dummy variables for Council Districts'''
+    # set what we are going to create these dummies from
+    dummy_df =  pd.get_dummies(df['Council District'])
+    # Name the new columns
+    dummy_df.columns = ['district_0', 'district_1', 'district_2', 
+                        'district_3', 'district_4', 'district_5',
+                        'district_6', 'district_7', 'district_8',
+                        'district_9', 'district_10']
+    # add the dummies to the data frame
+    df = pd.concat([df, dummy_df], axis=1)
     return df
 
 #-----------------------------------------------------------------------------
@@ -140,7 +176,9 @@ def clean_column_names(df):
                     'CaseStatus': 'case_status', 'SourceID':'source_id', 'XCOORD': 'longitude', 'YCOORD': 'latitude',
                     'Report Starting Date': 'report_start_date', 'Report Ending Date': 'report_end_date'
                       })
-    df['zipcode'] = df['address'].str.extract(r'(\d{5}\-?\d{0,4})')
+    df['zipcode'] = df['address'].str.extract(r'.*(\d{5}?)$')  
+    #drop zipcode nulls after obtaining zipcode
+    df.dropna(subset=['zipcode'], how='all', inplace=True)         
     return df
 
 #-----------------------------------------------------------------------------
@@ -154,9 +192,102 @@ def clean_311(df):
     df = handle_nulls(df)
     # creating delay involved columns
     df = create_delay_columns(df)
+    # handle outliers
+    df = handle_outliers(df)
+    # make dummies
+    df = create_dummies(df)
     # merge reasons
     df = clean_reason(df)
     # rename columns
     df = clean_column_names(df)
+    df.to_csv('clean_311.csv')
     # return df
     return df
+
+
+# Train/Split the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def split(df, stratify_by= 'level_of_delay'):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=319)
+        train, validate = train_test_split(train, test_size=.3, random_state=319)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=319, stratify=df[stratify_by])
+        train, validate = train_test_split(train, test_size=.3, random_state=319, stratify=train[stratify_by])
+    return train, validate, test
+
+# Create X_train, y_train, etc...~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def separate_y(train, validate, test):
+    '''
+    This function will take the train, validate, and test dataframes and separate the target variable into its
+    own panda series
+    '''
+    
+    X_train = train.drop(columns=['level_of_delay'])
+    y_train = train.level_of_delay
+    X_validate = validate.drop(columns=['level_of_delay'])
+    y_validate = validate.level_of_delay
+    X_test = test.drop(columns=['level_of_delay'])
+    y_test = test.level_of_delay
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+# Scale the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def scale_data(X_train, X_validate, X_test):
+    '''
+    This function will scale numeric data using Min Max transform after 
+    it has already been split into train, validate, and test.
+    '''
+    
+    
+    obj_col = ['open_date', 'due_date', 'closed_date', 'is_late', 'dept', 'call_reason', 'case_type', 'case_status', 'source_id', 'address', 'zipcode']
+    num_train = X_train.drop(columns = obj_col)
+    num_validate = X_validate.drop(columns = obj_col)
+    num_test = X_test.drop(columns = obj_col)
+    
+    
+    # Make the thing
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    
+   
+    # we only .fit on the training data
+    scaler.fit(num_train)
+    train_scaled = scaler.transform(num_train)
+    validate_scaled = scaler.transform(num_validate)
+    test_scaled = scaler.transform(num_test)
+    
+    # turn the numpy arrays into dataframes
+    train_scaled = pd.DataFrame(train_scaled, columns=num_train.columns)
+    validate_scaled = pd.DataFrame(validate_scaled, columns=num_train.columns)
+    test_scaled = pd.DataFrame(test_scaled, columns=num_train.columns)
+    
+    
+    return train_scaled, validate_scaled, test_scaled
+
+# Combo Train & Scale Function~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def split_separate_scale(df, stratify_by= 'level_of_delay'):
+    '''
+    This function will take in a dataframe
+    separate the dataframe into train, validate, and test dataframes
+    separate the target variable from train, validate and test
+    then it will scale the numeric variables in train, validate, and test
+    finally it will return all dataframes individually
+    '''
+    
+    # split data into train, validate, test
+    train, validate, test = split(df, stratify_by= 'level_of_delay')
+    
+     # seperate target variable
+    X_train, y_train, X_validate, y_validate, X_test, y_test = separate_y(train, validate, test)
+    
+    
+    # scale numeric variable
+    train_scaled, validate_scaled, test_scaled = scale_data(X_train, X_validate, X_test)
+    
+    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test, train_scaled, validate_scaled, test_scaled
